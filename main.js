92bae57e0a67b89978b4d152eb10de200c28e7e2
Kallurisravani12 <KalluriSravani12>
2023-04-14 15:36:33 +0200

    My first commit

---
+++ main.js
@@ -0,0 +1,53 @@
+//For Loop
+/*for (var i=0;i<=20;i++){
+    console.log(i);
+}*/
+//While loop
+/*var i= 1;
+while(i<=20){
+    console.log(i);
+    i++;
+}*/
+
+//Do while loop
+/*var i= 1;
+do{
+    console.log(i);
+    i++;
+}while(i<=20)*/
+
+/*//For Loop for checking even
+for (var i=0;i<=20;i++){
+    if (i%2==0){
+        console.log(i + ' '+'is even');
+    }
+}*/
+
+
+//While loop for checking even numbers
+/*var i= 1;
+while(i<=20){
+    if (i%2==0){
+    console.log(i);
+   
+    }  
+     i++; 
+}*/
+
+//do While loop for checking even numbers
+/*var i= 1;
+		do{
+		    if (i%2==0){
+		    	console.log(i +" "+ "is even" );
+		    
+		}
+		    i++;
+		}while(i<=20);*/
+
+
+var i=0,s=0;
+for (var i=1;i<=10;i++){
+    s=i*9;
+    console.log(s);
+
+}
